4f8ca54817a52f15f713c811f6f28c210bd29f3e
Marian <maryan.zozulia@gmail.com>
2020-08-11 20:08:13 +0200

    Code Review, spelling correction, style correction

--- weight.py
+++ weight.py
@@ -1,34 +1,33 @@
+# coding=utf-8
+
 import argparse
 
 
-def createparser():
+def create_parser():
     parser = argparse.ArgumentParser(
         prog='weight_ratio',
-        description='cool program'
+        description='This program checks your weight'
     )
     parser.add_argument('-w', '--weight', type=int, metavar='')
     parser.add_argument('-hh', '--height', type=float, metavar='')
     return parser
 
 
-def start_parcing():
-    parser = createparser()
-    return parser.parse_args()
+def start_parsing():
+    return create_parser().parse_args()
 
 
 def check_weight():
-    parser = start_parcing()
-    kef = parser.weight / (parser.height * parser.height)
-    return kef
+    parser = start_parsing()
+    return parser.weight / parser.height ** 2
 
 
 def comparator():
-    parser = start_parcing()
-    kefi = check_weight()
+    kefi = check_weight() # what does kefi men ?
     if kefi < 18.5:
         print('Low weight')
     elif 18.5 < kefi < 24.9:
-        print('Eveything is fine!')
+        print('Everything is fine!')
     elif 25.0 < kefi < 29.9:
         print('Dangerous!')
     elif 29.9 < kefi < 34.9:
