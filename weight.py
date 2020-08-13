41a2d4f19dcf38094175224bb0a83b8660aa5b85
Egor <fatamorganaa933@gmail.com>
2020-08-13 14:56:59 +0300

    Add logger

--- weight.py
+++ weight.py
@@ -1,4 +1,11 @@
 import argparse
+import logging
+
+
+def create_logger():
+    logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%H:%M:%S', level='INFO')
+    logger = logging.getLogger()
+    return logger
 
 
 def createparser():
@@ -23,18 +30,28 @@ def check_weight():
 
 
 def comparator():
+    answear = create_logger()
+    answear.info('Start programm...')
     parser = start_parcing()
     kefi = check_weight()
     if kefi < 18.5:
-        print('Low weight')
+        answear.info('Low weight')
     elif 18.5 < kefi < 24.9:
-        print('Eveything is fine!')
+        answear.info('Everything is fine!')
     elif 25.0 < kefi < 29.9:
-        print('Dangerous!')
+        answear.info('Dangerous!')
     elif 29.9 < kefi < 34.9:
-        print('How can you move?')
+        answear.info('How can you move?')
     elif 35.9 < kefi:
-        print('OMG')
+        answear.info('OMG')
+    answear.info('Do you want some advice?')
+    advice = input()
+    if advice == 'yes':
+        answear.info('okay')
+    elif advice == 'no':
+        answear.info('''that's all''')
+    else:
+        answear.warning('You can use only yes/no')
 
 
 if __name__ == '__main__':
